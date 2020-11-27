f414d153198e4bcb00da5dc4d991f60ebe88d57d
Edin Jašarević <edinjasar14@gmail.com>
2020-11-27 22:26:52 +0100

    create a test project

---
+++ WiredBrainCoffee.CupOrderAdmin.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
