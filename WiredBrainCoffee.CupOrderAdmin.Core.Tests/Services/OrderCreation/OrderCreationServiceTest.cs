940842bc801989c9c78de83d620f0e0828d72eb2
Edin Jašarević <edinjasar14@gmail.com>
2020-11-27 22:37:00 +0100

    add the orderCreationServiceTest class

---
+++ WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
+{
+    [TestClass]
+    public class OrderCreationServiceTest
+    {
+        [TestMethod]
+        public void ShouldStoreCreatedOrderInORderCreateionResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
