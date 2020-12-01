cb4fe9f72e4b56591809711f4422ee28e0aee765
Edin Jašarević <edinjasar14@gmail.com>
2020-12-01 21:40:19 +0100

    implement your first test method

--- WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTest.cs
+++ WiredBrainCoffee.CupOrderAdmin.Core.Tests/Services/OrderCreation/OrderCreationServiceTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WiredBrainCoffee.CupOrderAdmin.Core.Model;
+using WiredBrainCoffee.CupOrderAdmin.Core.Services.OrderCreation;
 
 namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
 {
@@ -7,9 +10,18 @@ namespace WiredBrainCoffee.CupOrderAdmin.Core.Tests.Services.OrderCreation
     public class OrderCreationServiceTest
     {
         [TestMethod]
-        public void ShouldStoreCreatedOrderInORderCreateionResult()
+        public async  Task ShouldStoreCreatedOrderInORderCreateionResult()
         {
-            throw new NotImplementedException();
+            var orderCreationService = new OrderCreationService(null, null);
+
+            var numberOfOrderedCups = 1;
+            var customer = new Customer() {Id = 99};
+
+            var orderCreationResult = await orderCreationService.CreateOrderAsync(customer, numberOfOrderedCups);
+
+            Assert.AreEqual(OrderCreationResultCode.Success, orderCreationResult.ResultCode);
+            Assert.IsNotNull(orderCreationResult.CreatedOrder);
+            Assert.AreEqual(customer.Id, orderCreationResult.CreatedOrder.CustomerId);
         }
     }
 }
